dd4ed56fb8e81f879d3bc307a183f03b5c11885d
shiaiwen <2474577454@qq.com>
2022-11-27 22:37:32 +0800

    `faet(app.jsx):项目初始化`

--- react-scaffolding/src/App.js
+++ react-scaffolding/src/App.js
@@ -1,23 +1,36 @@
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import List from "./components/List";
+class App extends Component {
+  render() {
+    return (
+      <div className="todo-container">
+        <div className="todo-wrap">
+          <Header />
+          <List />
+          <Footer />
+        </div>
+      </div>
+    );
+  }
 }
 
+// 组件是 jsx
+
 export default App;
+
+// 拆分组件  拆分界面 抽取组件
+
+// 实现静态组件，实现组件的静态页面效果
+
+// 实现动态组件
+
+//  数据类型
+//  数据名称
+//  保存在哪个组件
+
+// 交互
+
+// 从绑定事件监听开始
